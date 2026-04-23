69bacb01c1b52f678fbe6a2f66317444db9cbc5f
Chung-heon <yi940715@gmail.com>
2026-04-23 17:08:58 +0900

    feat: Google Analytics 4 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Providers } from './providers';
 
@@ -35,6 +36,13 @@ export default function RootLayout({
             </head>
             <body>
                 <Providers>{children}</Providers>
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-1BKDMP0QGM" strategy="afterInteractive" />
+                <Script id="ga4" strategy="afterInteractive">{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-1BKDMP0QGM');
+                `}</Script>
             </body>
         </html>
     );
